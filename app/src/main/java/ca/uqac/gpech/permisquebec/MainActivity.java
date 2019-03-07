d94cebf9dbd18f6b970f3d67f8e0fb5d3ff55a88
Guilhem PECH <guilhempech@gmail.com>
2019-03-06 21:10:49 -0500

    part two

--- app/src/main/java/ca/uqac/gpech/permisquebec/MainActivity.java
+++ app/src/main/java/ca/uqac/gpech/permisquebec/MainActivity.java
@@ -2,6 +2,12 @@ package ca.uqac.gpech.permisquebec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Switch sexeSwitch = findViewById(R.id.switch_sexe);
+        CheckBox message = findViewById(R.id.paiement_exige);
+
+        if(message != null)
+        message.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView messageText = findViewById(R.id.textmessage);
+                if(isChecked){
+                    messageText.setActivated(true);
+                } else {
+                    messageText.setActivated(false);
+                }
+            }
+        });
+
+        if(sexeSwitch != null)
+        sexeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView v = findViewById(R.id.sexetext);
+                if(isChecked){
+                    v.setText(getString(R.string._h));
+                } else{
+                    v.setText(getString(R.string.f));
+                }
+            }
+        });
+    }
+
+    public void misaAJour(View view) {
+        EditText editNom = findViewById(R.id.textView_lastname);
+        EditText editPrenom = findViewById(R.id.textView_firstname);
+
+        TextView prenom = findViewById(R.id.prenomtext);
+        TextView nom = findViewById(R.id.nomText);
+
+        if(editNom.getText().length() != 0){
+            nom.setText(editNom.getText());
+        }
+
+        if(editPrenom.getText().length() != 0){
+            prenom.setText(editPrenom.getText());
+        }
+
+    }
+
+
+
+
 }
